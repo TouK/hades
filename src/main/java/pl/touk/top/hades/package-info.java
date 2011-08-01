c33dc4794d8b18a26b8eafffbb5dd867e70458a2
Michal Sokolowski <msk@touk.pl>
2011-08-01 10:28:48 +0000

    package comments added

---
+++ src/main/java/pl/touk/top/hades/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Provides a HA (high-availability) data source. This HA data source contains two other data sources - the main one
+ * and the failover one. The high-availability effect is achieved by switching between the two data sources. For
+ * example if the main data source breaks, the failover one replaces it until the situation is back to normal.
+ * <p>
+ * The process of switching between the two contained data sources is not controlled by the HA data source itself.
+ * Implementations of {@link pl.touk.top.hades.FailoverActivator} are responsible for this. Every HA data source is
+ * associated with such an implementation which activates failover (switch from the main data source to the failover
+ * one) or failback (the opposite switch) when some conditions are met. The conditions may vary between implementations.
+ * For example {@link pl.touk.top.hades.load.LoadFailoverActivator} activates failover when the main data source is
+ * overloaded.
+ */
+package pl.touk.top.hades;
